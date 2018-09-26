961205a225fb2fc5ac995019cc452850396a6984
Yoshitaka Terazawa <locol6622@gmail.com>
2018-09-26 10:23:51 +0900

    feat: update npm modules

--- src/libs/fetchApi.js
+++ src/libs/fetchApi.js
@@ -6,9 +6,12 @@ const wait = ms => new Promise(r => setTimeout(r, ms))
 
 const callApi = id => new Promise(r => r(`Your ID is ${id}`))
 
-const dataResource = createResource(([id, waitMs]) => {
-  return Promise.all([callApi(id), wait(waitMs)])
-}, ([id]) => id)
+const dataResource = createResource(
+  ([id, waitMs]) => {
+    return Promise.all([callApi(id), wait(waitMs)])
+  },
+  ([id]) => id
+)
 
 const getData = (id, waitMs) => dataResource.read(cache, [id, waitMs])
 
